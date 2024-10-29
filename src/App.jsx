3e3e6de83ae6ba5834f2f40d11d35e46cc101047
sathish19961412 <sathishfreelanc5@gmail.com>
2024-10-29 17:04:35 +0530

    create reactjs and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl font-thin underline jvlcode-red'>Hello JVLcode!</h1>
+    </>
+  )
+}
+
+export default App
